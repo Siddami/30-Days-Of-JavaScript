93c2e6185e86c2e636097a678da88d98b68b1436
Siddami <dharmmy621@gmail.com>
2025-10-20 17:02:41 +0100

    feat: added necessary js solutions to each js file

---
+++ 01_Day_Introduction/01_day_starter/datatype.js
@@ -0,0 +1,8 @@
+import { name, age, isFemale, nothing} from './variable.js'
+
+
+// 5. Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
+console.log(typeof name)
+console.log(typeof isFemale)
+console.log(typeof age)
+console.log(typeof nothing)
--- 01_Day_Introduction/01_day_starter/main.js
+++ 01_Day_Introduction/01_day_starter/main.js
@@ -1,4 +1,48 @@
-// the variable values can be accessed from other variable.js file
-console.log(firstName, lastName, country, city, age, isMarried) 
-console.log(gravity, boilingPoint, PI) // 9.81, 100, 3.14
-console.log(name, job, live)
+// 1. Write a single line comment which says, comments can make code readable
+
+    //Answer: comments can make code readable
+
+//  2. Write another single comment which says, Welcome to 30DaysOfJavaScript
+    
+    //Answer: Welcome to 30DaysOfJavaScript
+
+// 3. Write a multiline comment which says, comments can make code readable, easy to reuse and informative
+
+    /*
+        Answer:
+        comments can make code readable, 
+        easy to reuse and informative
+    */
+
+// 6. Declare four variables without assigning values
+
+let firstName;
+let lastName;
+let country;
+let city;
+
+// 7. Declare four variables with assigned values
+
+let language = "JavaScript";
+let job = "Developer";
+let isMarried = false;
+let year = 2024;
+
+// 8. Declare variables to store your first name, last name, marital status, country and age in multiple lines
+
+let myFirstName = "Yetunde";
+let myLastName = "Abdulkareem";
+let myMaritalStatus = "Single";
+let myCountry = "Kenya";
+let moiAge = 27;
+
+// 9. Declare variables to store your first name, last name, marital status, country and age in a single line
+
+let fName = "Yetunde", lName = "Abdulkareem", maritalStatus = " Single", myCountry2 = "Nigeria", myAge2 = 27;
+
+// 10. Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+
+let myAge = 27;
+let yourAge = 30;
+
+console.log("I", "am", myAge, "years old.\nYou", "are", yourAge, "years old.")
